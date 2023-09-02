5f673df0540541aa687187077edd36cee0f73511
Fyodor Davidov <Fedor.Davidov@honeywell.com>
2023-09-03 00:14:07 +0500

    Добавлены докстринги

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+"""Модуль игры."""
 from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str, char_class: str) -> str:
+    """Атака персонажа."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон'
                 f'противнику равный {5 + randint(3, 5)}')
@@ -15,6 +17,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Защита персонажа."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +28,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Специальный удар."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное'
                 f'умение «Выносливость {80 + 25}»')
@@ -36,6 +40,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Тренировка персонажа: фукнции атаки, защиты и спец атаки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +65,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Начало игры, выберите персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
